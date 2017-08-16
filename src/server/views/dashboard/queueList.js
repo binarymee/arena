c014c7e052345c00ed998b40ad81dd378bddabab
BinaryMee <binarymee@gmail.com>
2017-08-16 19:38:55 +0530

    Extended changes

--- src/server/views/dashboard/queueList.js
+++ src/server/views/dashboard/queueList.js
@@ -1,7 +1,17 @@
-function handler(req, res) {
+async function handler(req, res) {
   const {Queues} = req.app.locals;
-  const queues = Queues.list();
-
+  const queuesList = Queues.list();
+  const queues = [];
+  for( let queueMeta of queuesList) {
+  	try{
+  		const queue = await Queues.get(queueMeta.name, queueMeta.host);
+  		queueMeta.jobCounts = await queue.getJobCounts();	
+      queues.push(queueMeta);	
+  	} catch(err) {
+  		console.log(err);
+  	}  	
+  }
+  console.log(queues);
   return res.render('dashboard/templates/queueList', { queues });
 }
 
